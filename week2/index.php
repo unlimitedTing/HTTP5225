5ac9bcd38026e852959f3b0c17a861452c049f28
Ting Liu <liuttting0207@gmail.com>
2024-05-15 20:34:58 -0400

    add week2 in-class

---
+++ week2/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Week 2</title>
+</head>
+<body>
+    
+    <?php
+        echo '<h1>Welcome\' to HTTP 5225</h1>';
+    ?>
+    <?php
+        echo '<p>Let\'s Learn Together</p>';
+    ?>
+    
+    <?php
+        echo '<ul>
+                <li>PHP</li>
+                <li>Laravel</li>
+            </ul>';
+        $fname = 'Gary';
+        $lname = 'Bhanot';
+        $name['fname'] = 'Gary';
+        $name['lname'] = 'Bhanot';
+        echo 'Hello ' ,$fname,' ',$name['lname'];
+    ?>
+</body>
+</html>
